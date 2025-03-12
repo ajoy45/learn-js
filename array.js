6b4168d46cc9990c6114a000302df169fabff622
ajoy45 <ajoypust@gmail.com>
2025-03-13 00:22:01 +0600

    array done

---
+++ array.js
@@ -0,0 +1,59 @@
+// page 93
+1.
+const numbers = [71, 72, 73, 74, 75, 76, 77, 78, 78, 79];
+2.
+const fruits = ["apple", "banana", "pehara", "orange", "mango"];
+// page 96
+1.
+const friends = ["rana", "kana", "bana", "mala", "cala"];
+console.log(friends[3]);
+2.
+const books = ["java", "html", "css", "python", "node", "mopngodb", "js"];
+console.log(books[5]);
+3.
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+num[6] = 30;
+console.log(num);
+// page 99
+1.
+const age = [10, 20, 30, 40, 50];
+age.push(60);
+console.log(age)
+2.
+const friendss = ["sojib", "sagor", "sakib", "sakib", "shohel"];
+friendss.push("sumon");
+console.log(friendss);
+3.
+const games = ["papji", "freefire", "tiktok"];
+games.pop();
+console.log(games);
+4.
+const num1 = [24, 36, 48, 60];
+num1.unshift(12);
+console.log(num1)
+5.
+const fiveBook = ["java", "html", "css", "python", "node"];
+fiveBook.shift();
+console.log(fiveBook)
+// page 102
+1.
+const myFruits = ["mango", "apple", "banana", "louche"];
+if (myFruits.includes("mango")) {
+    console.log("mango ase")
+}
+else {
+    console.log("mango nai gache otto")
+}
+2.
+const names = ["babul", "alif", "soton"];
+const indexNum = names.indexOf("babul");
+console.log(indexNum);
+4.
+const city = ["Dhaka", "Chittagong", "Sylhet"];
+city.push("rajshahi");
+const exits = city.includes("Rajshahi");
+console.log(exits)
+
+
+
+
